e0a4a2516a7e6be71519fa53b264ffdb09d418c3
Marcel Stolin <kuri-dv@mailbox.org>
2023-03-18 11:06:10 +0100

    Updated watch-script to also start chatterino and run both apps with nohup in background

--- scripts/watch-twitch.sh
+++ scripts/watch-twitch.sh
@@ -2,16 +2,18 @@
 
 if [ -z "$1" ]
   then
-    echo "No stream URL provided."
+    echo "No channel name provided."
     exit 1
 fi
 
-url="${1}"
+channel="${1}"
+url="https://www.twitch.tv/${channel}"
 stream="${2:-480p,720p,1080p60,best}"
 player="${3:-mpv --profile='stream'}"
 buffer="${4:-64M}"
 
-streamlink --player="${player}" \
-           --ringbuffer-size="${buffer}" \
-           --twitch-low-latency \
-           -Q ${url} ${stream} &
+cmd_streamlink="streamlink -Q -p='${player}' --ringbuffer-size='${buffer}' --twitch-low-latency ${url} ${stream}"
+cmd_chatterino="chatterino -c '${channel}'"
+
+nohup sh -c "${cmd_streamlink}" > /dev/null 2>&1 &
+nohup sh -c "${cmd_chatterino}" > /dev/null 2>&1 &
